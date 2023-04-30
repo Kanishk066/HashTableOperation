d15b11b0abd855fb440a798ebe20cc578e0a7c56
Kanishk <kanishksinghal06@gmail.com>
2023-04-30 08:48:12 +0530

    Added the Welcome message to the main branch

---
+++ src/com/bl/hashtables/HashTableOperation.java
@@ -0,0 +1,7 @@
+package com.bl.hashtables;
+
+public class HashTableOperation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hash Table Program");
+    }
+}
